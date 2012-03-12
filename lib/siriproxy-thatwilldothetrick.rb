b49c3be53df6be50814fe1d5bb49552800286b3e
StephaneAG <seedsodesign@gmail.com>
2012-03-13 00:51:54 +0100

    debugging serial comm
    and arduino prog...

--- lib/siriproxy-thatwilldothetrick.rb
+++ lib/siriproxy-thatwilldothetrick.rb
@@ -348,16 +348,12 @@ class SiriProxy::Plugin::ThatWillDoTheTrick < SiriProxy::Plugin
     	port = SerialPort.new(@port_str, @baud_rate, @data_bits, @stop_bits, @parity) # create an instance of the serialport
 
     		sleep(5)
-    		port.write "light off" # set the light on on the Arduino ledPin
+    		port.write "hello" # set the light on on the Arduino ledPin
     		#printf("%s", port.gets) # print an output to the console
     		arduino_callback = port.gets # stock callback in the callback var
     		
     		sleep(5) # sleep for seconds, just to make sure the callback was succefully printed to the serial
-    	
-    	#port.close # close the serialport instance
-    		
-    	
-    	
+    	#port.close
     	say arduino_callback # alert user with callback from the arduino
     	request_completed #finally ,complete the request
   	end
@@ -366,58 +362,39 @@ class SiriProxy::Plugin::ThatWillDoTheTrick < SiriProxy::Plugin
   	
   
   	#arduino ledPin on
-  	listen_for /six make it bright/i do
-  	
-  	
-#  		port_str = '/dev/tty.usbmodem3a21'
-#		baud_rate = 9600
-#		data_bits = 8
-#		stop_bits = 1
-#		parity = SerialPort::NONE
-#  		
-#  		@arduino_callback = "hey!"
-    	
-    	#port = SerialPort.new(port_str, baud_rate, data_bits, stop_bits, parity) # create an instance of the serialport
-    	port = SerialPort.new(@port_str, @baud_rate, @data_bits, @stop_bits, @parity) # create an instance of the serialport
-    	
-    	while true do # while found, read forever
+  	listen_for /six can you feel it/i do
+  		
+  		port = SerialPort.new(@port_str, @baud_rate, @data_bits, @stop_bits, @parity) # create an instance of the serialport
+
     		sleep(5)
     		port.write "light on" # set the light on on the Arduino ledPin
-    		printf("%s", port.gets) # print an output to the console
-    		@arduino_callback = port.gets # stock callback in the callback var
+    		#printf("%s", port.gets) # print an output to the console
+    		arduino_callback = port.gets # stock callback in the callback var
     		
     		sleep(5) # sleep for seconds, just to make sure the callback was succefully printed to the serial
-    	end
-    	
-    	port.close # close the serialport instance
-    		
-    	
-    	
-    	say "Arduino left a message for you:" +  @arduino_callback # alert user with callback from the arduino
+    	#port.close
+    	say arduino_callback # alert user with callback from the arduino
     	request_completed #finally ,complete the request
+  		
   	end
   	
   	
   	
   	#arduino ledPin off
-  	listen_for /six stop the device/i do
+  	listen_for /six i feel good now/i do
     	
-    	port = SerialPort.new(port_str, baud_rate, data_bits, stop_bits, parity) # create an instance of the serialport
-    	
-    	while true do # while found, read forever
+    	port = SerialPort.new(@port_str, @baud_rate, @data_bits, @stop_bits, @parity) # create an instance of the serialport
+
+    		sleep(5)
     		port.write "light off" # set the light on on the Arduino ledPin
-    		printf("%s", port.gets) # print an output to the console
-    		@arduino_callback = port.gets # stock callback in the callback var
+    		#printf("%s", port.gets) # print an output to the console
+    		arduino_callback = port.gets # stock callback in the callback var
     		
-    		sleep(1) # sleep for a second, just to make sure the callback was succefully printed to the serial
-    	end
-    	
-    	port.close # close the serialport instance
-    		
-    	
-    	
-    	say "Arduino left a message for you:" +  @arduino_callback # alert user with callback from the arduino
+    		sleep(5) # sleep for seconds, just to make sure the callback was succefully printed to the serial
+    	#port.close
+    	say arduino_callback # alert user with callback from the arduino
     	request_completed #finally ,complete the request
+    	
   	end
   	
   
@@ -439,3 +416,37 @@ class SiriProxy::Plugin::ThatWillDoTheTrick < SiriProxy::Plugin
   end
 end
 
+
+
+
+#  	arduino ledPin on
+#  	listen_for /six make it bright/i do
+#  	
+#  	
+#  		#port_str = '/dev/tty.usbmodem3a21'
+#		#baud_rate = 9600
+#		#data_bits = 8
+#		#stop_bits = 1
+#		#parity = SerialPort::NONE
+#  		
+#  		#@arduino_callback = "hey!"
+#    	
+#    	#port = SerialPort.new(port_str, baud_rate, data_bits, stop_bits, parity) # create an instance of the serialport
+#    	port = SerialPort.new(@port_str, @baud_rate, @data_bits, @stop_bits, @parity) # create an instance of the serialport
+#    	
+#    	while true do # while found, read forever
+#    		sleep(5)
+#    		port.write "light on" # set the light on on the Arduino ledPin
+#    		printf("%s", port.gets) # print an output to the console
+#    		@arduino_callback = port.gets # stock callback in the callback var
+#    		
+#    		sleep(5) # sleep for seconds, just to make sure the callback was succefully printed to the serial
+#    	end
+#    	
+#    	port.close # close the serialport instance
+#    		
+#    	
+#    	
+#    	say "Arduino left a message for you:" +  @arduino_callback # alert user with callback from the arduino
+#    	request_completed #finally ,complete the request
+#  	end
